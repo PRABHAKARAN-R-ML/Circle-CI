25feccb5460322454c3cc86852a85af5273c60f1
PRABHAKARAN <100403697+PRABHAKARAN-R-ML@users.noreply.github.com>
2023-04-12 13:40:35 +0530

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -1,5 +1,5 @@
 import pytest 
 from app import app 
 def test_connection():
-    response = app.test_client().get("/")
+    response = app.test_client().get("/data/create")
     assert response.status_code == 200
